72b521d546a31d72b058729a7e30c2a7bf677866
Kamel Malki <kamelmalk96@gmail.com>
2022-10-18 16:35:47 +0200

    remove secret key

--- djangolearning/settings.py
+++ djangolearning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .secret import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-koq)=$z3o8=04_y=ucen-&lub=_122snj7xm8m-@5g_t7hf*^8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
